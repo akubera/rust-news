1f3883ac688fad3dcf688b774d9a2a5c2bf4e778
Andrew Kubera <andrew.michael.kubera@floss.email>
2017-01-17 20:13:58 -0500

    (public_html) Formatting fixes

--- src/public_html/index.rs
+++ src/public_html/index.rs
@@ -1,32 +1,30 @@
 
 use horrorshow::prelude::*;
 
-pub fn
-make_html() -> String
+pub fn make_html() -> String
 {
 
-    let s = html! {
-
+  let s = html! {
     b { : "something!" }
-}.into_string().unwrap();
-
-let res = html! {
-html {
-  head {
-    title { : "Rust News Roundup!" }
-    meta(charset="utf-8");
-    link(rel="stylesheet", href="reveal.min.css")
   }
-  body {
-    div(class="reveal") { div(class="slides") {
+            .into_string()
+            .unwrap();
 
+  let res = html! {
+    html {
+      head {
+        meta(charset="utf-8");
+        title: "Rust News Roundup!";
+        link(rel="stylesheet", href="reveal.min.css");
+      }
+      body {
+        div(class="reveal") {
+          div(class="slides") { : Raw(s) }
+        }
+      }
+    }
+  };
 
-    : raw! {s}
-
-    } }
-  } // body
-}
-}.into_string().unwrap();
-
-return res;
+  return res.into_string()
+            .unwrap();
 }
